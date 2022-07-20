5d10891685bd26e40f325707f600fc7279d967c2
EdsonFufu <edsonfufu@NMB-HQ101-MAC046.local>
2022-07-20 21:28:44 +0300

    Commit Created Index Page/Home page with MainController

---
+++ src/main/java/com/simplilearn/cicidemo/controllers/MainController.java
@@ -0,0 +1,20 @@
+package com.simplilearn.cicidemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping("/")
+    public ModelAndView index(){
+        ModelAndView modelAndView = new ModelAndView("index.html");
+        modelAndView.addObject("message","Welcome CI/CD Demo");
+        modelAndView.addObject("title","CI/CD Demo");
+        return modelAndView;
+    }
+
+}
